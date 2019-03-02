7e726a31245ba252dcca08db006a535a31eaff1a
John Puskas <john.h.puskas@gmail.com>
2019-03-02 16:52:18 -0600

    Added sort functionality for ViewSongs. Paging and search next.

--- MeMiXReMiX/Controllers/HomeController.cs
+++ MeMiXReMiX/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using MeMiXReMiX.ViewModels;
 using MeMiXReMiX.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
+using MeMiXReMiX.Helpers;
 
 namespace MeMiXReMiX.Controllers
 {
@@ -53,10 +54,29 @@ namespace MeMiXReMiX.Controllers
 
 
         [Authorize]
-        public IActionResult ViewSongs(string userName)
+        public IActionResult ViewSongs(string userName, string sortOrder)
         {
             string UserName = userName;
             IEnumerable<Song> AllSongs = context.Songs;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    AllSongs = AllSongs.OrderByDescending(s => s.ApplicationUserUserName);
+                    break;
+                case "name_asc":
+                    AllSongs = AllSongs.OrderBy(s => s.ApplicationUserUserName);
+                    break;
+                case "title_desc":
+                    AllSongs = AllSongs.OrderByDescending(s => s.Title);
+                    break;
+                case "title_asc":
+                    AllSongs = AllSongs.OrderBy(s => s.Title);
+                    break;
+                default:
+                    AllSongs = AllSongs.OrderBy(s => s.ID);
+                    break;
+            }
 
             if (userName == null)
             {
